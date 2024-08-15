f214320aa9fe6b3ae56a8707fef3ed2bc266f8b1
Hasee-hub <3377047801@qq.com>
2024-08-15 15:06:23 +0800

    scripts 2.0

--- scripts/main.js
+++ scripts/main.js
@@ -1,9 +1,28 @@
-let myHeading = document.querySelector("h1");
-myHeading.textContent = "Hello world!";
-let myVariable;
-myVariable = "李雷";
 alert("Welcome to my website!");
-document.querySelector("html").addEventListener("click", function () {
-    alert("别戳我，我怕疼。");
-  });
+let myButton = document.querySelector("button");
+let myHeading = document.querySelector("h1");
+
+myButton.onclick = function () {
+    setUserName();
+  };
+  
+function setUserName() {
+    let myName = prompt("请输入你的名字。");
+    if (!myName) {
+      setUserName();
+    } else {
+      localStorage.setItem("name", myName);
+      myHeading.textContent = "Mozilla 酷毙了，" + myName;
+    }
+  }
+
+if (!localStorage.getItem("name")) {
+    setUserName();
+  } else {
+    let storedName = localStorage.getItem("name");
+    myHeading.textContent = "Mozilla 酷毙了，" + storedName;
+  }
+  
+
+  
   
