a99db278cbbd56007ef527dc3587122d78dbc188
Hasee-hub <3377047801@qq.com>
2024-08-15 14:44:45 +0800

    add scripts

---
+++ scripts/main.js
@@ -0,0 +1,9 @@
+let myHeading = document.querySelector("h1");
+myHeading.textContent = "Hello world!";
+let myVariable;
+myVariable = "李雷";
+alert("Welcome to my website!");
+document.querySelector("html").addEventListener("click", function () {
+    alert("别戳我，我怕疼。");
+  });
+  
